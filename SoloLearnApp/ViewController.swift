a4e37a5fb9ed9915eff733cc33c4eded0c259275
Cristian Acuña <cristian@macbook-pro-de-cristian.local>
2017-07-11 11:41:35 -0400

    Initial Commit

---
+++ SoloLearnApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoloLearnApp
+//
+//  Created by Cristian Acuña on 11-07-17.
+//  Copyright © 2017 Nihilism Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
